107d8061701acd1b1fe82b882c98bcb13dc9b5eb
森下勇樹 <>
2025-02-02 21:18:12 +0900

    update routes for devise_token_auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
   resources :tasks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth'
+    end
+  end
 end
