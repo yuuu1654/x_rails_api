de52abf95c9449fe99044aa6dabcfa23a68de54e
森下勇樹 <>
2025-02-02 20:40:12 +0900

    update inheritance to enable devise_token_auth

--- app/controllers/api/v1/auth/registrations_controller.rb
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -1,2 +1,3 @@
-class Api::V1::Auth::RegistrationsController < ApplicationController
+# devise_token_authで予め用意されている認証用のコントローラを継承させる
+class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
 end
