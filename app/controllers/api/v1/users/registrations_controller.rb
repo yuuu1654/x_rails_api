4de8f8a5f98d1d805a988ba52ef21abfd61dc211
森下勇樹 <>
2025-02-03 21:19:13 +0900

    rails g controller api/v1/auth/registrations

---
+++ app/controllers/api/v1/users/registrations_controller.rb
@@ -0,0 +1,8 @@
+class Api::V1::Users::RegistrationsController < DeviseTokenAuth::RegistrationsController
+
+  # private
+
+  # def sign_up_params
+  #   params.permit(%i[email password password_confirmation name])
+  # end
+end
