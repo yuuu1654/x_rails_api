20db515e63c58521f5fb65a885fb9df869f52f8b
森下勇樹 <>
2025-02-09 20:22:03 +0900

    fix typo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@
 class ApplicationController < ActionController::API
         include DeviseTokenAuth::Concerns::SetUserByToken
         before_action do
-                I18n.local = :ja
+                I18n.locale = :ja
         end
 end
