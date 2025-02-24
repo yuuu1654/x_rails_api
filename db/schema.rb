1e20591f99b23ac63386eeffefc0bd360f646e3b
森下勇樹 <>
2025-02-24 20:08:19 +0900

    add columns to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_02_02_110939) do
+ActiveRecord::Schema[7.0].define(version: 2025_02_24_064545) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -41,6 +41,13 @@ ActiveRecord::Schema[7.0].define(version: 2025_02_02_110939) do
     t.json "tokens"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "phone"
+    t.date "birthday"
+    t.text "bio"
+    t.string "location"
+    t.string "website_url"
+    t.string "avatar_url"
+    t.string "header_url"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
