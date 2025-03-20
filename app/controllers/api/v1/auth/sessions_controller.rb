b91960d4dc212279740df2b1b97c4e08f69769e0
森下勇樹 <>
2025-03-20 20:26:02 +0900

    activate session

--- app/controllers/api/v1/auth/sessions_controller.rb
+++ app/controllers/api/v1/auth/sessions_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::Auth::SessionsController < ApplicationController
   def index
-    if current_api_v1_user
+    if current_api_v1_user # ここでセッションを使用
       render status: :ok, json: { is_login: true, data: current_api_v1_user }
     else
       render status: :unauthorized, json: { is_login: false, message: "ユーザーが存在しません" }
--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,11 @@ module Myapp
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
+    # ログイン情報を保存する際にセッションを使用 (APIモードではデフォルトでセッションを使用していない為)
+    config.session_store :cookie_store, key: '_interslice_session'
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use config.session_store, config.session_options
+
     config.generators do |g|
       g.test_framework :rspec,
                        fixtures: false,
